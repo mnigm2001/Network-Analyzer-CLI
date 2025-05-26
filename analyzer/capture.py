7743212100fbd21b7ab60c8b9d336d6b2d2ca662
mnigm2001 <mnigm2001@gmail.com>
2025-05-26 00:02:05 -0400

    Project Setup

--- analyzer/capture.py
+++
@@ -1,6 +0,0 @@
-from scapy.all import sniff, wrpcap
-
-def capture_packets(interface="eth0", count=50, output_file="data/capture.pcap"):
-    packets = sniff(count=count, iface=interface)
-    wrpcap(output_file, packets)
-    print(f"[+] Captured {count} packets and saved to {output_file}")
